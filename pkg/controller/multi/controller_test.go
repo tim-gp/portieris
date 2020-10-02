96268393900dbbb8e55da1af29b34bb32296499a
Alex Naylor <Alexander.Naylor@ibm.com>
2020-10-02 17:58:27 +0100

    Add more tests for getPatchesForContainers but no multiple loops

--- pkg/controller/multi/controller_test.go
+++ pkg/controller/multi/controller_test.go
@@ -21,68 +21,96 @@ import (
 
 	"github.com/IBM/portieris/helpers/credential"
 	"github.com/IBM/portieris/helpers/image"
-	securityenforcementv1beta1 "github.com/IBM/portieris/pkg/apis/securityenforcement/v1beta1"
+	"github.com/IBM/portieris/pkg/apis/securityenforcement/v1beta1"
 	"github.com/IBM/portieris/pkg/verifier/vulnerability"
 	"github.com/IBM/portieris/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	k8sv1beta1 "k8s.io/api/admission/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 type mockPolicyClient struct {
 	mock.Mock
 }
 
-func (mpc *mockPolicyClient) GetPolicyToEnforce(namespace, image string) (*securityenforcementv1beta1.Policy, error) {
+func (mpc *mockPolicyClient) GetPolicyToEnforce(namespace, image string) (*v1beta1.Policy, error) {
 	args := mpc.Called(namespace, image)
-	return args.Get(0).(*securityenforcementv1beta1.Policy), args.Error(1)
+	return args.Get(0).(*v1beta1.Policy), args.Error(1)
 }
 
 type mockEnforcer struct {
 	mock.Mock
 }
 
-func (me *mockEnforcer) DigestByPolicy(namespace string, img *image.Reference, credentials credential.Credentials, policy *securityenforcementv1beta1.Policy) (*bytes.Buffer, error, error) {
+func (me *mockEnforcer) DigestByPolicy(namespace string, img *image.Reference, credentials credential.Credentials, policy *v1beta1.Policy) (*bytes.Buffer, error, error) {
 	args := me.Called(namespace, img, credentials, policy)
 	return args.Get(0).(*bytes.Buffer), args.Error(1), args.Error(2)
 }
 
-func (me *mockEnforcer) VulnerabilityPolicy(img *image.Reference, credentials credential.Credentials, policy *securityenforcementv1beta1.Policy) vulnerability.ScanResponse {
+func (me *mockEnforcer) VulnerabilityPolicy(img *image.Reference, credentials credential.Credentials, policy *v1beta1.Policy) vulnerability.ScanResponse {
 	args := me.Called(img, credentials, policy)
 	return args.Get(0).(vulnerability.ScanResponse)
 }
 
+type mockKubeWrapper struct {
+	mock.Mock
+	kubernetes.Interface
+}
+
+func (mkw *mockKubeWrapper) GetPodSpec(req *k8sv1beta1.AdmissionRequest) (string, *corev1.PodSpec, error) {
+	args := mkw.Called(req)
+	return args.String(0), args.Get(1).(*corev1.PodSpec), args.Error(2)
+}
+
+func (mkw *mockKubeWrapper) GetSecretToken(namespace, secretName, registry string) (string, string, error) {
+	args := mkw.Called(namespace, secretName, registry)
+	return args.String(0), args.String(1), args.Error(2)
+}
+
+func (mkw *mockKubeWrapper) GetSecretKey(namespace, secretName string) ([]byte, error) {
+	args := mkw.Called(namespace, secretName)
+	return args.Get(0).([]byte), args.Error(1)
+}
+
+func (mkw *mockKubeWrapper) GetBasicCredentials(namespace, secretName string) (string, string, error) {
+	args := mkw.Called(namespace, secretName)
+	return args.String(0), args.String(1), args.Error(2)
+}
+
 func TestController_getPatchesForContainers(t *testing.T) {
 	type getPolicyToEnforceMock struct {
-		inImage   string
-		outPolicy *securityenforcementv1beta1.Policy
+		outPolicy *v1beta1.Policy
 		outErr    error
 	}
 	type enforcerVulnerabilityPolicyMock struct {
-		inImage         image.Reference
-		inCredentials   credential.Credentials
 		outScanResponse vulnerability.ScanResponse
 	}
 	type enforceDigestByPolicyMock struct {
-		inImage       image.Reference
-		inCredentials credential.Credentials
-		outDigest     string
-		outDeny       error
-		outErr        error
+		outDigest string
+		outDeny   error
+		outErr    error
+	}
+	type mocks struct {
+		getPolicyToEnforce          *getPolicyToEnforceMock
+		inImage                     string
+		credentials                 credential.Credentials
+		enforcerVulnerabilityPolicy *enforcerVulnerabilityPolicyMock
+		enforceDigestByPolicy       *enforceDigestByPolicyMock
 	}
 	tests := []struct {
-		name                             string
-		containerType                    string
-		namespace                        string
-		specPath                         string
-		imagePullSecrets                 []corev1.LocalObjectReference
-		containers                       []corev1.Container
-		getPolicyToEnforceMocks          []getPolicyToEnforceMock
-		enforcerVulnerabilityPolicyMocks []enforcerVulnerabilityPolicyMock
-		enforceDigestByPolicyMocks       []enforceDigestByPolicyMock
-		wantPatches                      []types.JSONPatch
-		wantDenials                      []string
-		wantErr                          error
+		name             string
+		containerType    string
+		namespace        string
+		specPath         string
+		imagePullSecrets []corev1.LocalObjectReference
+		containers       []corev1.Container
+		mocks            []mocks
+		wantPatches      []types.JSONPatch
+		wantDenials      []string
+		wantErr          error
 	}{
 		{
 			name:        "No containers, return no patches or denials",
@@ -106,10 +134,12 @@ func TestController_getPatchesForContainers(t *testing.T) {
 			containers: []corev1.Container{
 				{Image: "icr.io/some-namespace/image:tag"},
 			},
-			getPolicyToEnforceMocks: []getPolicyToEnforceMock{
+			mocks: []mocks{
 				{
 					inImage: "icr.io/some-namespace/image:tag",
-					outErr:  fmt.Errorf("no sorry"),
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outErr: fmt.Errorf("no sorry"),
+					},
 				},
 			},
 			wantPatches: []types.JSONPatch{},
@@ -122,54 +152,261 @@ func TestController_getPatchesForContainers(t *testing.T) {
 			containers: []corev1.Container{
 				{Image: "icr.io/some-namespace/image:tag"},
 			},
-			getPolicyToEnforceMocks: []getPolicyToEnforceMock{
+			mocks: []mocks{
 				{
 					inImage: "icr.io/some-namespace/image:tag",
-					outErr:  fmt.Errorf("no sorry"),
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outErr: fmt.Errorf("no sorry"),
+					},
 				},
 			},
 			wantPatches: []types.JSONPatch{},
 			wantDenials: []string{"no sorry"},
 			wantErr:     nil,
 		},
+		{
+			name:      "Not vulnerable with no signing enforcement",
+			namespace: "some-namespace",
+			imagePullSecrets: []corev1.LocalObjectReference{
+				{
+					Name: "wibble",
+				},
+			},
+			containers: []corev1.Container{
+				{Image: "icr.io/some-namespace/image:tag"},
+			},
+			mocks: []mocks{
+				{
+					inImage: "icr.io/some-namespace/image:tag",
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outPolicy: &v1beta1.Policy{},
+					},
+					credentials: credential.Credentials{{
+						Username: "wibble",
+						Password: "dibble",
+					}},
+					enforcerVulnerabilityPolicy: &enforcerVulnerabilityPolicyMock{
+						outScanResponse: vulnerability.ScanResponse{
+							CanDeploy: true,
+						},
+					},
+					enforceDigestByPolicy: &enforceDigestByPolicyMock{},
+				},
+			},
+			wantPatches: []types.JSONPatch{},
+			wantDenials: []string{},
+			wantErr:     nil,
+		},
+		{
+			name:      "digest by policy errors",
+			namespace: "some-namespace",
+			imagePullSecrets: []corev1.LocalObjectReference{
+				{
+					Name: "wibble",
+				},
+			},
+			containers: []corev1.Container{
+				{Image: "icr.io/some-namespace/image:tag"},
+			},
+			mocks: []mocks{
+				{
+					inImage: "icr.io/some-namespace/image:tag",
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outPolicy: &v1beta1.Policy{},
+					},
+					credentials: credential.Credentials{{
+						Username: "wibble",
+						Password: "dibble",
+					}},
+					enforcerVulnerabilityPolicy: &enforcerVulnerabilityPolicyMock{
+						outScanResponse: vulnerability.ScanResponse{
+							CanDeploy: true,
+						},
+					},
+					enforceDigestByPolicy: &enforceDigestByPolicyMock{
+						outErr: fmt.Errorf("failed"),
+					},
+				},
+			},
+			wantPatches: []types.JSONPatch{},
+			wantDenials: []string{},
+			wantErr:     fmt.Errorf("failed"),
+		},
+		{
+			name:      "digest by policy says denied",
+			namespace: "some-namespace",
+			imagePullSecrets: []corev1.LocalObjectReference{
+				{
+					Name: "wibble",
+				},
+			},
+			containers: []corev1.Container{
+				{Image: "icr.io/some-namespace/image:tag"},
+			},
+			mocks: []mocks{
+				{
+					inImage: "icr.io/some-namespace/image:tag",
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outPolicy: &v1beta1.Policy{},
+					},
+					credentials: credential.Credentials{{
+						Username: "wibble",
+						Password: "dibble",
+					}},
+					enforcerVulnerabilityPolicy: &enforcerVulnerabilityPolicyMock{
+						outScanResponse: vulnerability.ScanResponse{
+							CanDeploy: true,
+						},
+					},
+					enforceDigestByPolicy: &enforceDigestByPolicyMock{
+						outDeny: fmt.Errorf("I don't think so"),
+					},
+				},
+			},
+			wantPatches: []types.JSONPatch{},
+			wantDenials: []string{"I don't think so"},
+			wantErr:     nil,
+		},
+		{
+			name:      "digest by policy returns a digest",
+			namespace: "some-namespace",
+			imagePullSecrets: []corev1.LocalObjectReference{
+				{
+					Name: "wibble",
+				},
+			},
+			containers: []corev1.Container{
+				{Image: "icr.io/some-namespace/image:tag"},
+			},
+			mocks: []mocks{
+				{
+					inImage: "icr.io/some-namespace/image:tag",
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outPolicy: &v1beta1.Policy{},
+					},
+					credentials: credential.Credentials{{
+						Username: "wibble",
+						Password: "dibble",
+					}},
+					enforcerVulnerabilityPolicy: &enforcerVulnerabilityPolicyMock{
+						outScanResponse: vulnerability.ScanResponse{
+							CanDeploy: true,
+						},
+					},
+					enforceDigestByPolicy: &enforceDigestByPolicyMock{
+						outDigest: "somedigest",
+					},
+				},
+			},
+			wantPatches: []types.JSONPatch{},
+			wantDenials: []string{},
+			wantErr:     nil,
+		},
+		{
+			name:      "allowed by signing but vulnerable",
+			namespace: "some-namespace",
+			imagePullSecrets: []corev1.LocalObjectReference{
+				{
+					Name: "wibble",
+				},
+			},
+			containers: []corev1.Container{
+				{Image: "icr.io/some-namespace/image:tag"},
+			},
+			mocks: []mocks{
+				{
+					inImage: "icr.io/some-namespace/image:tag",
+					getPolicyToEnforce: &getPolicyToEnforceMock{
+						outPolicy: &v1beta1.Policy{},
+					},
+					credentials: credential.Credentials{{
+						Username: "wibble",
+						Password: "dibble",
+					}},
+					enforcerVulnerabilityPolicy: &enforcerVulnerabilityPolicyMock{
+						outScanResponse: vulnerability.ScanResponse{
+							CanDeploy:  false,
+							DenyReason: "I don't want to",
+						},
+					},
+					enforceDigestByPolicy: &enforceDigestByPolicyMock{
+						outDigest: "somedigest",
+					},
+				},
+			},
+			wantPatches: []types.JSONPatch{},
+			wantDenials: []string{"I don't want to"},
+			wantErr:     nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			podSpec := corev1.PodSpec{
+				ImagePullSecrets: tt.imagePullSecrets,
+			}
+
 			policyClient := mockPolicyClient{}
 			policyClient.Test(t)
 			defer policyClient.AssertExpectations(t)
-			for _, gptem := range tt.getPolicyToEnforceMocks {
-				policyClient.
-					On("GetPolicyToEnforce", tt.namespace, gptem.inImage).
-					Return(gptem.outPolicy, gptem.outErr).Once()
-			}
+
+			kubeWrapper := mockKubeWrapper{}
+			kubeWrapper.Test(t)
+			defer kubeWrapper.AssertExpectations(t)
 
 			enforcer := mockEnforcer{}
 			enforcer.Test(t)
 			defer enforcer.AssertExpectations(t)
-			for idx, evpm := range tt.enforcerVulnerabilityPolicyMocks {
-				//TODO getting the policy like this is not great
-				policy := tt.getPolicyToEnforceMocks[idx].outPolicy
-				enforcer.
-					On("VulnerabilityPolicy", evpm.inImage, evpm.inCredentials, policy).
-					Return(evpm.outScanResponse).Once()
-			}
-			for idx, edbpm := range tt.enforceDigestByPolicyMocks {
-				policy := tt.getPolicyToEnforceMocks[idx].outPolicy
-				digest := &bytes.Buffer{
-					//todo use edbpm.outDigest
+
+			for idx, m := range tt.mocks {
+				imageName := m.inImage
+				var img *image.Reference
+				if imageName != "" {
+					var err error
+					img, err = image.NewReference(imageName)
+					require.NoError(t, err)
 				}
-				enforcer.On("DigestByPolicy", tt.namespace, edbpm.inImage, edbpm.inCredentials, policy).
-					Return(digest, edbpm.outDeny, edbpm.outErr)
+				policy := m.getPolicyToEnforce.outPolicy
+				namespace := tt.namespace
+				if m.getPolicyToEnforce != nil {
+					err := m.getPolicyToEnforce.outErr
+					policyClient.
+						On("GetPolicyToEnforce", namespace, img.String()).Return(policy, err).Once()
+				}
+
+				creds := m.credentials
+				for _, cred := range creds {
+					//TODO don't mock.Anything
+					kubeWrapper.On("GetSecretToken", namespace, mock.Anything, img.GetHostname()).
+						Return(cred.Username, cred.Password, nil).Once()
+				}
+
+				if m.enforcerVulnerabilityPolicy != nil {
+					response := m.enforcerVulnerabilityPolicy.outScanResponse
+					enforcer.On("VulnerabilityPolicy", img, creds, policy).Return(response).Once()
+				}
+
+				if m.enforceDigestByPolicy != nil {
+					var digest *bytes.Buffer
+					if m.enforceDigestByPolicy.outDigest != "" {
+						digest = &bytes.Buffer{}
+						digest.Write([]byte(m.enforceDigestByPolicy.outDigest))
+						wantPatch := types.JSONPatch{
+							Op:    "replace",
+							Path:  fmt.Sprintf("%s/%s/%d/image", tt.specPath, tt.containerType, idx),
+							Value: fmt.Sprintf("%s@sha256:%s", img.NameWithoutTag(), m.enforceDigestByPolicy.outDigest),
+						}
+						tt.wantPatches = append(tt.wantPatches, wantPatch)
+					}
+					deny := m.enforceDigestByPolicy.outDeny
+					err := m.enforceDigestByPolicy.outErr
+					enforcer.On("DigestByPolicy", namespace, img, creds, policy).Return(digest, deny, err).Once()
+				}
 			}
 
 			c := &Controller{
-				policyClient: &policyClient,
-				Enforcer:     &enforcer,
-			}
-
-			podSpec := corev1.PodSpec{
-				ImagePullSecrets: tt.imagePullSecrets,
+				policyClient:         &policyClient,
+				Enforcer:             &enforcer,
+				kubeClientsetWrapper: &kubeWrapper,
 			}
 
 			gotPatches, gotDenials, gotErr := c.getPatchesForContainers(tt.containerType, tt.namespace, tt.specPath, podSpec, tt.containers)
